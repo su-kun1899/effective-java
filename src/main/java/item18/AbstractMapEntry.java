d7d4b2cbef34d51689713517e63d62275943cf6f
su-kun1899 <higedrum.coz@gmail.com>
2016-10-10 00:56:27 +0900

    項目18 抽象クラスよりインターフェースを選ぶ

---
+++ src/main/java/item18/AbstractMapEntry.java
@@ -0,0 +1,44 @@
+package item18;
+
+import java.util.Map;
+
+// 骨格実装
+public abstract class AbstractMapEntry<K, V> implements Map.Entry<K, V> {
+
+  // 基本操作
+  public abstract K getKey();
+
+  public abstract V getValue();
+
+  // 変更可能なマップでのエントリーは、このメソッドをオーバライドしなければならない
+  public V setValue(V value) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
+    }
+    if (!(obj instanceof Map.Entry)) {
+      return false;
+    }
+    Map.Entry<?, ?> arg = (Map.Entry<?, ?>) obj;
+    return equals(getKey(), arg.getKey()) &&
+        equals(getValue(), arg.getValue());
+  }
+
+  private static boolean equals(Object obj1, Object obj2) {
+    return obj1 == null ? obj2 == null : obj1.equals(obj2);
+  }
+
+  // Map.Entry.hashCode の一般契約を実装
+  @Override
+  public int hashCode() {
+    return hashCode(getKey()) ^ hashCode(getValue());
+  }
+
+  private static int hashCode(Object obj) {
+    return obj == null ? 0 : obj.hashCode();
+  }
+}
