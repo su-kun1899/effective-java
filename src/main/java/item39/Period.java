220d53c98d0e008ced7b24bd81cc28817344c149
su-kun1899 <higedrum.coz@gmail.com>
2016-11-09 01:09:16 +0900

    項目39 必要な場合には、防御的にコピーする

---
+++ src/main/java/item39/Period.java
@@ -0,0 +1,38 @@
+package item39;
+
+import java.util.Date;
+
+/**
+ * 期間クラス
+ * <p>Dateはlongで扱うほうが適切かもしれない
+ */
+public final class Period {
+  private final Date start;
+  private final Date end;
+
+  /**
+   * @param start 期間の開始
+   * @param end 期間の終わりで、開始より前であってはならない
+   * @throws IllegalArgumentException startがendの後の場合
+   * @throws NullPointerException startかendがnullの場合
+   */
+  public Period(Date start, Date end) {
+    // パラメータの防御的コピーをする
+    this.start = new Date(start.getTime());
+    this.end = new Date(end.getTime());
+
+    if (start.compareTo(end) > 0) {
+      throw new IllegalArgumentException(this.start + " after " + this.end);
+    }
+  }
+
+  public Date start() {
+    // 内部データの防御的コピー
+    return new Date(start.getTime());
+  }
+
+  public Date end() {
+    // 内部データの防御的コピー
+    return new Date(end.getTime());
+  }
+}
